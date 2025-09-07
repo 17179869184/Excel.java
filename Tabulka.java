74f4f8083f65aa4a127fdb2a4f98b655a14b513f
ExcelMaster <88095744+17179869184@users.noreply.github.com>
2025-09-07 23:25:17 +0200

    Vylepšení

--- Tabulka.java
+++ Tabulka.java
@@ -11,7 +11,7 @@ import javax.swing.JTextArea;
 class Tabulka {
     Bunka[][] bunky = new Bunka[16][16];
     String pismena = "ABCDEFGHIJKLMNOP";
-    JFrame okno = new JFrame("Excel");
+    JFrame okno = new JFrame("Excel.java");
     JTextArea textAreaTabulka = new JTextArea(10, 40);
     int[] velikosti = new int[16];
 
@@ -19,8 +19,8 @@ class Tabulka {
         File soubor = new File("sesit.xcl");
         try {
             Scanner cist = new Scanner(soubor);
-            for (int radek = 0; radek < 16; radek++) {
-                for (int sloupec = 0; sloupec < 16; sloupec++) {
+            for (byte radek = 0; radek < 16; radek++) {
+                for (byte sloupec = 0; sloupec < 16; sloupec++) {
                     bunky[radek][sloupec] = new Bunka();
                     String data = cist.nextLine();
                     bunky[radek][sloupec].hodnota = data.split("\t")[0];
@@ -33,22 +33,23 @@ class Tabulka {
         }
 
         sirkaSloupcu();
-        okno.setSize(1300, 600);
+        okno.setSize(1400, 600);
         okno.getContentPane().add("Center", textAreaTabulka);
         okno.setVisible(true);
         textAreaTabulka.setFont(new Font("Courier New", 0, 12));
         textAreaTabulka.setBackground(Color.BLACK);
         textAreaTabulka.setForeground(Color.WHITE);
+        textAreaTabulka.setCaretColor(Color.GREEN);
+        textAreaTabulka.setSelectionColor(Color.GREEN);
         //textAreaTabulka.setEnabled(false);
         print();
         okno.addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent windowEvent) {
                 String text = textAreaTabulka.getText();
                 okno.dispose();
-                System.out.println("Nezavírat program! Stále se ukládá!");
                 text = text.split("\\|", 19)[18];
-                int sloupec = -1;
-                int radek = 0;
+                byte sloupec = -1;
+                byte radek = 0;
                 for (String bunka: text.split("\\|")) {
                     if (sloupec == 16) {
                         sloupec = -1;
@@ -56,26 +57,25 @@ class Tabulka {
                     }
                     sloupec++;
                     if (!bunka.contains("\n")) {
-                        prepsat(pismena.charAt(sloupec) + "", radek, bunka.trim());
+                        prepsat(sloupec, radek, bunka.trim());
                     }
                 }
                 ulozit();
-                System.out.println("Sešit se uložil!");
             }
         });
     }
 
     public void print() {
         String strTabulka = "   |";
-        for (int sloupec = 0; sloupec < 16; sloupec++) { // názvy sloupců
+        for (byte sloupec = 0; sloupec < 16; sloupec++) { // názvy sloupců
             int sirka = (velikosti[sloupec] + 1) / 2;
             strTabulka += " ".repeat(sirka) + pismena.charAt(sloupec) + " ".repeat(velikosti[sloupec] - sirka + 1) + "|";
         }
 
         strTabulka += "\n";
-        for (int radek = 0; radek < 16; radek++) { // tabulka
+        for (byte radek = 0; radek < 16; radek++) { // tabulka
             strTabulka += " ".repeat(2 - String.valueOf(radek).length()) + radek + " |";
-            for (int sloupec = 0; sloupec < 16; sloupec++) {
+            for (byte sloupec = 0; sloupec < 16; sloupec++) {
                 Bunka bunka = bunky[radek][sloupec];
                 int sirka = velikosti[sloupec] - bunka.hodnota.length() + 1;
                 if (bunka.zarovnani == -1) {
@@ -95,19 +95,18 @@ class Tabulka {
 
     public void sirkaSloupcu() {
         velikosti = new int[16];
-        for (int radek = 0; radek < 16; radek++) { // výpočet šířky sloupců
-            for (int sloupec = 0; sloupec < 16; sloupec++) {
+        for (byte radek = 0; radek < 16; radek++) { // výpočet šířky sloupců
+            for (byte sloupec = 0; sloupec < 16; sloupec++) {
                 velikosti[sloupec] = Math.max(5, Math.max(velikosti[sloupec], bunky[radek][sloupec].hodnota.length()));
             }
         }
     }
 
-    public void prepsat(String sloupec, int radek, String hodnota) {
-        bunky[radek][pismena.indexOf(sloupec)].hodnota = hodnota.replace("\n", "").replace("\t", "");
-        sirkaSloupcu();
+    public void prepsat(byte sloupec, byte radek, String hodnota) {
+        bunky[radek][sloupec].hodnota = hodnota.replace("\t", "");
     }
 
-    public void zarovnej(String sloupec, int radek, byte zarovnani) {
+    public void zarovnej(String sloupec, byte radek, byte zarovnani) {
         bunky[radek][pismena.indexOf(sloupec)].zarovnani = zarovnani;
         ulozit();
     }
@@ -115,8 +114,8 @@ class Tabulka {
     public void ulozit() {
         try {
             FileWriter zapsat = new FileWriter("sesit.xcl");
-            for (int radek = 0; radek < 16; radek++) {
-                for (int sloupec = 0; sloupec < 16; sloupec++) {
+            for (byte radek = 0; radek < 16; radek++) {
+                for (byte sloupec = 0; sloupec < 16; sloupec++) {
                     zapsat.write(bunky[radek][sloupec].hodnota + "\t" + bunky[radek][sloupec].zarovnani + "\n");
                 }
             }
